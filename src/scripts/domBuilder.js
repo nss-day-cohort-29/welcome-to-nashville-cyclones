885b94810376b6175e3152a6d0e3080d9205f864
Jordan Rosas <jordanrosas1203@gmail.com>
2018-12-07 15:24:24 -0600

    tried to add input fields- figured it out with hunter

---
+++ src/scripts/domBuilder.js
@@ -0,0 +1,8 @@
+const domBuilder = {
+    searchInput(){
+        let searchInputField = document.createElement('form');
+        searchInputField.innerHTML = `
+            <input type = "text">
+        `
+    } 
+}
