7c5e28fb287fa6f11ec2e8da9d5a276cf8da140e
Meg Cruzen <megcruzen@gmail.com>
2018-12-11 11:44:46 -0600

    Added dropdown to parks search and adjusted line-height

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -25,14 +25,23 @@ const domBuilder = {
             <option>Tennis Courts</option>
             <option>Walking/Jogging Paths</option>
         </select>
-        <button type="button" id="parksButton">LETS GO!</button> <br>
+        <br>
+        <button type="button" id="parksButton">LETS GO!</button>
+        <br>
 
-        <input type = "text" placeholder = "Restaurants by food type" id = "restaurantSearch"><button type = button id = 'restaurantsButton'>LETS GO!</button> <br>
+        <input type="text" placeholder="Restaurants by cuisine" id="restaurantSearch">
+        <br>
+        <button type="button" id ="restaurantsButton">LETS GO!</button>
+        <br>
 
-        <input type = "text" placeholder = "Meet-ups by topic" id = "meetupSearch"><button type = button id = 'meetButton'>LETS GO!</button> <br>
+        <input type="text" placeholder="Meetups by topic" id="meetupSearch">
+        <br>
+        <button type="button" id="meetButton">LETS GO!</button>
+        <br>
 
-        <input type = "text" placeholder = "Concerts by genre" id = "concertSearch"><button type = button id = 'concertsButton'>LETS GO!</button>
-
+        <input type="text" placeholder="Concerts by genre" id="concertSearch">
+        <br>
+        <button type="button" id ="concertsButton">LETS GO!</button>
         </form>
         `
         // console.log(searchInputField);
