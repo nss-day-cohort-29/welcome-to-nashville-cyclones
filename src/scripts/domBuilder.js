3ddf1ab497a3e4a93ecf08aacdec11772a94e939
Meg Cruzen <megcruzen@gmail.com>
2018-12-11 10:50:42 -0600

    Added dropdown to parks search and adjusted line-height

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -4,7 +4,28 @@ const domBuilder = {
         `<h2>Search Nashville</h2>
         <form>
 
-        <input type = "text" placeholder = "Parks by feature" id = "parkSearch"><button type = button id = 'parksButton'>LETS GO!</button> <br>
+        <select name="parkSearch" id="parkSearch">
+            <option value="" selected disabled hidden>Parks by feature</option>
+            <option>ADA Accessible</option>
+            <option>Baseball Fields</option>
+            <option>Boat Launch</option>
+            <option>Camping Available</option>
+            <option>Community Center</option>
+            <option>Disc Golf</option>
+            <option>Dog Park</option>
+            <option>Fishing</option>
+            <option>Football Fields</option>
+            <option>Hiking Trails</option>
+            <option>Horse Trails</option>
+            <option>Lake</option>
+            <option>Playground</option>
+            <option>Restrooms Available</option>
+            <option>Skate Park</option>
+            <option>Swimming Pool</option>
+            <option>Tennis Courts</option>
+            <option>Walking/Jogging Paths</option>
+        </select>
+        <button type="button" id="parksButton">LETS GO!</button> <br>
 
         <input type = "text" placeholder = "Restaurants by food type" id = "restaurantSearch"><button type = button id = 'restaurantsButton'>LETS GO!</button> <br>
 
